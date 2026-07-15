59e72e9e5c47afb9f4db61ebc450d42acf0e8868
fahmiaqmar <fahmiaqmar9@gmail.com>
2026-07-15 11:27:11 +0800

    Create dashboard.js

---
+++ assets/js/dashboard.js
@@ -0,0 +1,49 @@
+
+let tickets =
+JSON.parse(localStorage.getItem("tickets")) || [];
+
+
+document.getElementById("total").innerHTML =
+tickets.length;
+
+
+document.getElementById("pending").innerHTML =
+tickets.filter(
+x=>x.status=="Pending"
+).length;
+
+
+
+document.getElementById("completed").innerHTML =
+tickets.filter(
+x=>x.status=="Completed"
+).length;
+
+
+
+new Chart(
+document.getElementById("ticketChart"),
+{
+
+type:"bar",
+
+data:{
+
+labels:[
+"Hardware",
+"Software",
+"Network",
+"Device"
+],
+
+datasets:[{
+
+label:"Ticket",
+
+data:[5,8,3,10]
+
+}]
+
+}
+
+});
