df07424e8c5cf6d325ed5d7f2fb7d0e868eb845f
fahmiaqmar <fahmiaqmar9@gmail.com>
2026-07-15 17:22:44 +0800

    Update dashboard.js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,7 +1,3 @@
-
-
-
-
 let tickets =
 JSON.parse(localStorage.getItem("tickets")) || [];
 
@@ -16,37 +12,7 @@ x=>x.status=="Pending"
 ).length;
 
 
-
 document.getElementById("completed").innerHTML =
 tickets.filter(
 x=>x.status=="Completed"
 ).length;
-
-
-
-new Chart(
-document.getElementById("ticketChart"),
-{
-
-type:"bar",
-
-data:{
-
-labels:[
-"Hardware",
-"Software",
-"Network",
-"Device"
-],
-
-datasets:[{
-
-label:"Ticket",
-
-data:[5,8,3,10]
-
-}]
-
-}
-
-});
